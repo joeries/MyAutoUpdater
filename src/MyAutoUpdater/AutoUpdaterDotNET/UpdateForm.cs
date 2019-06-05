9c533a068cfa021247724c50397106c909eaf842
Joeries Young <zofx@9icode.com>
2019-06-05 17:17:26 +0800

    update
    update

---
+++ src/MyAutoUpdater/AutoUpdaterDotNET/UpdateForm.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AutoUpdaterDotNET
+{
+    internal partial class UpdateForm : Form
+    {
+        private bool HideReleaseNotes { get; set; }
+
+        public UpdateForm()
+        {
+            InitializeComponent();
+            UseLatestIE();
+            buttonSkip.Visible = AutoUpdater.ShowSkipButton;
+            buttonRemindLater.Visible = AutoUpdater.ShowRemindLaterButton;
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
+            Text = string.Format(resources.GetString("$this.Text", CultureInfo.CurrentCulture),
+                AutoUpdater.AppTitle, AutoUpdater.CurrentVersion);
+            labelUpdate.Text = string.Format(resources.GetString("labelUpdate.Text", CultureInfo.CurrentCulture),
+                AutoUpdater.AppTitle);
+            labelDescription.Text =
+                string.Format(resources.GetString("labelDescription.Text", CultureInfo.CurrentCulture),
+                    AutoUpdater.AppTitle, AutoUpdater.CurrentVersion, AutoUpdater.InstalledVersion);
+            if (string.IsNullOrEmpty(AutoUpdater.ChangelogURL))
+            {
+                HideReleaseNotes = true;
+                var reduceHeight = labelReleaseNotes.Height + webBrowser.Height;
+                labelReleaseNotes.Hide();
+                webBrowser.Hide();
+
+                Height -= reduceHeight;
+
+                buttonSkip.Location = new Point(buttonSkip.Location.X, buttonSkip.Location.Y - reduceHeight);
+                buttonRemindLater.Location = new Point(buttonRemindLater.Location.X,
+                    buttonRemindLater.Location.Y - reduceHeight);
+                buttonUpdate.Location = new Point(buttonUpdate.Location.X, buttonUpdate.Location.Y - reduceHeight);
+            }
+        }
+
+        public sealed override string Text
+        {
+            get { return  base.Text; }
+            set { base.Text = value; }
+        }
+
+        private void UseLatestIE()
+        {
+            int ieValue = 0;
+            switch (webBrowser.Version.Major)
+            {
+                case 11:
+                    ieValue = 11001;
+                    break;
+                case 10:
+                    ieValue = 10001;
+                    break;
+                case 9:
+                    ieValue = 9999;
+                    break;
+                case 8:
+                    ieValue = 8888;
+                    break;
+                case 7:
+                    ieValue = 7000;
+                    break;
+            }
+            if (ieValue != 0)
+            {
+                using (RegistryKey registryKey =
+                    Registry.CurrentUser.OpenSubKey(
+                        @"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true))
+                {
+                    registryKey?.SetValue(Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName), ieValue,
+                        RegistryValueKind.DWord);
+                }
+            }
+        }
+
+        private void UpdateFormLoad(object sender, EventArgs e)
+        {
+            if (!HideReleaseNotes)
+            {
+                webBrowser.Navigate(AutoUpdater.ChangelogURL);
+            }
+        }
+
+        private void ButtonUpdateClick(object sender, EventArgs e)
+        {
+            if (AutoUpdater.OpenDownloadPage)
+            {
+                var processStartInfo = new ProcessStartInfo(AutoUpdater.DownloadURL);
+
+                Process.Start(processStartInfo);
+
+                DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                if (AutoUpdater.DownloadUpdate())
+                {
+                    DialogResult = DialogResult.OK;
+                }
+            }
+        }
+
+        private void ButtonSkipClick(object sender, EventArgs e)
+        {
+            using (RegistryKey updateKey = Registry.CurrentUser.CreateSubKey(AutoUpdater.RegistryLocation))
+            {
+                if (updateKey != null)
+                {
+                    updateKey.SetValue("version", AutoUpdater.CurrentVersion.ToString());
+                    updateKey.SetValue("skip", 1);
+                }
+            }
+        }
+
+        private void ButtonRemindLaterClick(object sender, EventArgs e)
+        {
+            if (AutoUpdater.LetUserSelectRemindLater)
+            {
+                var remindLaterForm = new RemindLaterForm();
+
+                var dialogResult = remindLaterForm.ShowDialog();
+
+                if (dialogResult.Equals(DialogResult.OK))
+                {
+                    AutoUpdater.RemindLaterTimeSpan = remindLaterForm.RemindLaterFormat;
+                    AutoUpdater.RemindLaterAt = remindLaterForm.RemindLaterAt;
+                }
+                else if (dialogResult.Equals(DialogResult.Abort))
+                {
+                    ButtonUpdateClick(sender, e);
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            using (RegistryKey updateKey = Registry.CurrentUser.CreateSubKey(AutoUpdater.RegistryLocation))
+            {
+                if (updateKey != null)
+                {
+                    updateKey.SetValue("version", AutoUpdater.CurrentVersion);
+                    updateKey.SetValue("skip", 0);
+                    DateTime remindLaterDateTime = DateTime.Now;
+                    switch (AutoUpdater.RemindLaterTimeSpan)
+                    {
+                        case RemindLaterFormat.Days:
+                            remindLaterDateTime = DateTime.Now + TimeSpan.FromDays(AutoUpdater.RemindLaterAt);
+                            break;
+                        case RemindLaterFormat.Hours:
+                            remindLaterDateTime = DateTime.Now + TimeSpan.FromHours(AutoUpdater.RemindLaterAt);
+                            break;
+                        case RemindLaterFormat.Minutes:
+                            remindLaterDateTime = DateTime.Now + TimeSpan.FromMinutes(AutoUpdater.RemindLaterAt);
+                            break;
+
+                    }
+                    updateKey.SetValue("remindlater",
+                        remindLaterDateTime.ToString(CultureInfo.CreateSpecificCulture("en-US").DateTimeFormat));
+                    AutoUpdater.SetTimer(remindLaterDateTime);
+                }
+            }
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void UpdateForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            AutoUpdater.Running = false;
+        }
+    }
+}
---
+++ src/MyAutoUpdater/ZipExtractor/FormMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace ZipExtractor
+{
+    public partial class FormMain : Form
+    {
+        private BackgroundWorker _backgroundWorker;
+        private string zipFile;
+        private string mianExeFile;
+
+        public FormMain(string _zipFile, string _mianExeFile)
+        {
+            InitializeComponent();
+            this.zipFile = _zipFile;
+            this.mianExeFile = _mianExeFile;
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            //foreach (var process in Process.GetProcesses())
+            //{
+            //    try
+            //    {
+            //        if (process.MainModule.FileName.Equals(mianExeFile))
+            //        {
+            //            labelInformation.Text = @"Waiting for application to Exit...";
+            //            process.WaitForExit();
+            //        }
+            //    }
+            //    catch (Exception exception)
+            //    {
+            //        Debug.WriteLine(exception.Message);
+            //    }
+            //}
+
+            // Extract all the files.
+            _backgroundWorker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+
+            _backgroundWorker.DoWork += (o, eventArgs) =>
+            {
+                var path = Path.GetDirectoryName(mianExeFile);
+
+                // Open an existing zip file for reading.
+                ZipStorer zip = ZipStorer.Open(zipFile, FileAccess.Read);
+
+                // Read the central directory collection.
+                List<ZipStorer.ZipFileEntry> dir = zip.ReadCentralDir();
+
+                for (var index = 0; index < dir.Count; index++)
+                {
+                    if (_backgroundWorker.CancellationPending)
+                    {
+                        eventArgs.Cancel = true;
+                        zip.Close();
+                        return;
+                    }
+                    ZipStorer.ZipFileEntry entry = dir[index];
+                    zip.ExtractFile(entry, Path.Combine(path, entry.FilenameInZip));
+                    _backgroundWorker.ReportProgress((index + 1) * 100 / dir.Count, string.Format(MyAutoUpdater.Properties.Resources.CurrentFileExtracting, entry.FilenameInZip));
+                }
+
+                zip.Close();
+            };
+
+            _backgroundWorker.ProgressChanged += (o, eventArgs) =>
+            {
+                progressBar.Value = eventArgs.ProgressPercentage;
+                labelInformation.Text = eventArgs.UserState.ToString();
+            };
+
+            _backgroundWorker.RunWorkerCompleted += (o, eventArgs) =>
+            {
+                if (!eventArgs.Cancelled)
+                {
+                    labelInformation.Text = @"Finished";
+                    //try
+                    //{
+                    //    ProcessStartInfo processStartInfo = new ProcessStartInfo(args[2]);
+                    //    if (args.Length > 3)
+                    //    {
+                    //        processStartInfo.Arguments = args[3];
+                    //    }
+                    //    Process.Start(processStartInfo);
+                    //}
+                    //catch (Win32Exception exception)
+                    //{
+                    //    if (exception.NativeErrorCode != 1223)
+                    //        throw;
+                    //}
+                    Close();
+                }
+            };
+            _backgroundWorker.RunWorkerAsync();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _backgroundWorker?.CancelAsync();
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
